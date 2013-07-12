7f12d921598de8ce8a26dbb2f0126ba96bf92d50
romKa919293 <romKa919293@mail.ru>
2013-07-12 04:27:56 +0400

    проект

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+class MyButtonClass : Form
+{
+private Button mrButton;
+// Метод-конструктор
+public MyButtonClass()
+{
+mrButton = new Button();
+mrButton.Text = 'Нажми меня';
+mrButton.Click += new System.EventHandler(MyButtonClickEventHandler);
+this.Controls.Add(mrButton);
+}
+// Основной метод
+static void Main()
+{
+Application.Run(new MyButtonClass());
+}
+// Метод-обработчик событий
+void MyButtonClickEventHandler(object sender, EventArgs e)
+{
+mrButton.Text = “Вы нажали меня!”;
+}
+}
